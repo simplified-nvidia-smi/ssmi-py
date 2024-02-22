65940a9e0678e61692a3d1b2ce31f2ce41a0843c
belfner <belfner@belfner.com>
2024-02-21 23:24:46 -0500

    added comments

--- ssmi.py
+++ ssmi.py
@@ -26,47 +26,77 @@ def nvidia_smi():
 
 
 def ssmi():
+    # get run flags from CLI
     args = parse_args()
     print_gpu_info = args.gpus
     print_proc_info = args.procs
 
+    # Execute the nvidia-smi command and open a stream to read its output
     nvidia_smi_output = nvidia_smi()
-    nvidia_smi_output.strip()
+
+    # split the output by lines and create an iterator
     lines = iter(nvidia_smi_output.split('\n'))
+
+    # The execution runs in a try/except structure to catch the StopIteration error that liens will throw when
+    # it has been fully consumed
     try:
+        # consume but do not print the time and date
         next(lines)
+
+        # Read the next three lines of the command's output and print each if not NULL
+        # +---------------------------------------------------------------------------------------+
+        # | NVIDIA-SMI 535.154.05             Driver Version: 535.154.05   CUDA Version: 12.2     |
+        # |-----------------------------------------+----------------------+----------------------+
         for _ in range(3):
             line = next(lines)
             if print_gpu_info:
                 print(line)
 
+        # Loops once per GPU on system
         while True:
-            line = next(lines)
 
+            # if the line starts with a space that means there are no more gpus to be processed
+            line = next(lines)
             if line[0] == ' ':
                 break
+
             if print_gpu_info:
-                print(line[:6], end='')
+                print(line[:6], end='')  # prints "| GPU "
+
             line = next(lines)
+
+            # prints " Fan  Temp " + " Perf " + "      Pwr:Usage/Cap |         Memory-Usage | GPU-Util  Compute M. |"
             if print_gpu_info:
                 print(line[1:12] + line[13:19] + line[23:])
 
+            # skip blank line
             next(lines)
+
+            # prints block seperator "...-----..."
             line = next(lines)
             if print_gpu_info:
                 print(line)
 
-        # next(lines)
+        # if the gpu details were not printed out print the header for the process table
         line = next(lines)
         if not print_gpu_info and print_proc_info:
             print(line)
 
+        # dump the line that just says processes
+        # "| Processes:
         next(lines)
+
+        # print the process table column headers
+        # "|  GPU   GI   CI        PID   Type   Process name                            GPU Memory |"
         line = next(lines)
         if print_proc_info:
             print(line)
+
+        # dump the line that has overflow text from the headders
+        # "|        ID   ID                                                             Usage      |"
         next(lines)
 
+        # print out the rest of the process table
         while True:
             line = next(lines)
             if print_proc_info:
