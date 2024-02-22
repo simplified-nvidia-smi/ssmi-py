38d60bb12dcee1589d1231050e9dc6aef0b691dc
belfner <belfner@belfner.com>
2024-02-21 21:49:22 -0500

    added argument parsing

--- ssmi.py
+++ ssmi.py
@@ -1,15 +1,34 @@
+import argparse
 from subprocess import Popen, PIPE
 
 MAX_ITERS = 1000
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Displays a simplified version of the output from the `nvidia-smi` command.\n'
+                    'When no options are provided, it displays both GPU details and process information.')
+    parser.add_argument('-g', '--gpus', action='store_true', help='display GPU details')
+    parser.add_argument('-p', '--procs', action='store_true', help='display process details')
+
+    args = parser.parse_args()
+
+    if not (args.gpus or args.procs):
+        args.gpus = args.procs = True
+
+    return args
+
+
 def nvidia_smi():
-    proc = Popen(['nvidia-smi'],stdout=PIPE,encoding='utf-8')
+    proc = Popen(['nvidia-smi'], stdout=PIPE, encoding='utf-8')
     stdout, _ = proc.communicate()
     return stdout
 
+
 def ssmi():
-    print_gpu_info = True
-    print_proc_info = True
+    args = parse_args()
+    print_gpu_info = args.gpus
+    print_proc_info = args.procs
 
     nvidia_smi_output = nvidia_smi()
     nvidia_smi_output.strip()
@@ -27,10 +46,10 @@ def ssmi():
             if line[0] == ' ':
                 break
             if print_gpu_info:
-                print(line[:6],end='')
+                print(line[:6], end='')
             line = next(lines)
             if print_gpu_info:
-                print(line[1:12]+line[13:19]+line[23:])
+                print(line[1:12] + line[13:19] + line[23:])
 
             next(lines)
             line = next(lines)
@@ -58,4 +77,6 @@ def ssmi():
     except StopIteration:
         pass
 
-ssmi()
+
+if __name__ == '__main__':
+    ssmi()
